edaf97b0e510b76226710db9e87b6397b3be7131
KocherSarah <Sarah.Kocher@stud.uni-regensburg.de>
2017-08-18 01:15:08 +0200

    Fancies

--- web_interface/data.js
+++ web_interface/data.js
@@ -1,6 +1,4 @@
-
-
-        request({
+ request({
           url: "http://127.0.0.1:5000/weights",
           method: "GET",
           success: (data) => pie_chart(JSON.parse(data)),
--- web_interface/pie_chart.js
+++ web_interface/pie_chart.js
@@ -1,9 +1,25 @@
 function pie_chart(input) {
     console.log(input);
+    var ultimateColors = [['rgb(146, 123, 21)', 'rgb(177, 180, 34)', 'rgb(206, 206, 40)', 'rgb(175, 51, 21)', 'rgb(35, 36, 21)']];
+
     var data = [{
         values: Object.values(input),
         labels: Object.keys(input),
-        type: 'pie'
+        type: 'pie',
+        marker: {
+            colors: ['#f46d43', '#fdae61', '#fee090', '#e0f3f8', '#abd9e9', '#74add1']
+        }
     }];
-    Plotly.newPlot('myDiv', data);
+
+    var layout = {
+    title: 'Gewichtung',
+    paper_bgcolor: '#ccdffa',
+    font:
+        {
+            color:'#555555'
+        }
+
+}
+
+    Plotly.newPlot('chartDiv', data, layout);
 }
