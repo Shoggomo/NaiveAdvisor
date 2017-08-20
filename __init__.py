b249ec37244eef8a193d40d44d0e51e3b0ee79ab
Shoggomo <andy-hot@web.de>
2017-08-20 20:53:42 +0200

    Finished Routes

--- __init__.py
+++ __init__.py
@@ -1,8 +1,13 @@
-import RestAPI
+import rest_api
+from core import supervisor
+
+
+def supervisor_ready(supervisor):
+    rest_api.run(supervisor)
 
 
 def main():
-    pass
+    supervisor.SuperVisor(supervisor_ready)
 
 if __name__ == "__main__":
     main()
--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -4,19 +4,24 @@ import json
 
 app = Flask(__name__)
 CORS(app)
+supervisor = None
 
 
 @app.route('/weights')
 def weights():
     data = {"Service": 1/6., "Cleanliness": 1/6., "Value": 1/6., "Sleep Quality": 1/6., "Rooms": 1/6., "Location": 1/6.} # mockdata
+    # data = supervisor.get_statistics()
     return json.dumps(data)
 
 
-@app.route('/classify', methods=['POST'])
+@app.route('/classify')
 def classify():
-    categories = ['service', 'cleanliness', 'value', 'sleep-quality', 'rooms', 'location']
-    features = {category: request.json[category] for category in categories}
-    # classify(service, ...)
-    return str(reduce(lambda pre, curr: pre + int(curr), features.values(), 0))  # mockdata
+    # Arguments must be: Service, Cleanliness, Value, Sleep Quality, Location, Rooms
+    features = {category: int(request.args[category]) for category in request.args}
+    return supervisor.classify(features)
 
-app.run(port=5000)
+
+def run(_supervisor):
+    global supervisor
+    supervisor = _supervisor
+    app.run(port=5000)
