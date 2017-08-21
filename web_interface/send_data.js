49bee9a7e94d84621bdc137a59ce8ae0d26dc9bf
KocherSarah <Sarah.Kocher@stud.uni-regensburg.de>
2017-08-21 22:25:49 +0200

    Delete send_data.js

--- web_interface/send_data.js
+++
@@ -1,8 +0,0 @@
-function sendData(string){
-request({
-          url: string, //hier gehört string hin
-          method: "GET",
-          success: (data) => document.querySelector('.result_number').innerHTML = data,
-          error: () => alert("Failed to connect to server!"),
- });
-}
