6bc49bc4b2c1f6afb230b2b8aaebf2e85477974c
KocherSarah <Sarah.Kocher@stud.uni-regensburg.de>
2017-08-21 22:25:16 +0200

    Delete data.js

--- web_interface/data.js
+++
@@ -1,6 +0,0 @@
- request({
-          url: "http://127.0.0.1:5000/weights",
-          method: "GET",
-          success: (data) => pie_chart(JSON.parse(data)),
-          error: () => alert("Failed to connect to server!"),
-        });
